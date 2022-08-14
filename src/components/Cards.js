de7d34d4598bba6eb79bd84bd8d086287379a3f6
overgrownFudge <overgrownfudge@gmail.com>
2022-08-13 21:13:00 -0400

    added location ping image to cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,15 +1,20 @@
 import React from "react"
+import ping_logo from "../images/location-ping.png"
+
 
 export default function Cards(props) {
     return (
         <div className="cards">
-            <div className = "images">
-                <img src={props.img} />
+            <div className="images">
+                <img src={props.img}/>
             </div>
-            <div className = "card">
-                <p>"logo" {props.location} <a href = {props.googleMaps}>View on Google Maps</a> </p>
+            <div className="card">
+                <p><img className= "ping-logo" src ={ping_logo}/>
+                    {props.location} 
+                    <a href={props.googleMaps}>View on Google Maps</a>
+                </p>
                 <h1>{props.title}</h1>
-                <p className = "dates">{props.start} - {props.end}</p>
+                <p className="dates">{props.start} - {props.end}</p>
                 <p>{props.description}</p>
             </div>
         </div>
