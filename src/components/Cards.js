6b6a02796b5512114b6b11a472dcd8af2885dc53
overgrownFudge <overgrownfudge@gmail.com>
2022-08-15 21:31:18 -0400

    removed location icon out of p element
    inserted all location info into one div

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,18 +1,18 @@
 import React from "react"
-import ping_logo from "../images/location-ping.png"
+import ping_icon from "../images/location-ping.png"
 
 
 export default function Cards(props) {
     return (
         <div className="cards">
             <div className="images">
-                <img src={props.img}/>
+                <img src={props.img} />
             </div>
             <div className="card">
-                <p><img className= "ping-logo" src ={ping_logo}/>
-                    {props.location} 
-                    <a href={props.googleMaps}>View on Google Maps</a>
-                </p>
+                <div className="location-info">
+                    <img className="ping-icon" src={ping_icon} />
+                    <p>{props.location} <a href={props.googleMaps}>View on Google Maps</a></p>
+                </div>
                 <h1>{props.title}</h1>
                 <p className="dates">{props.start} - {props.end}</p>
                 <p>{props.description}</p>
